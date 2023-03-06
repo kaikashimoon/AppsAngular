035085b7b5da5ca4cf533a0e30574abf12664397
kai <leonc4667@gmail.com>
2023-03-06 15:55:26 -0400

    angular

---
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Registro de usuarios';
+
+  mensaje = ''
+  registrado = false
+  nombre:string=''
+  apellido:string=''
+
+  registrarUsuario() {
+
+    this.registrado = true
+    this.mensaje = `Usuario registrado con exito, ${this.nombre} ${this.apellido}`
+  }
+
+}
